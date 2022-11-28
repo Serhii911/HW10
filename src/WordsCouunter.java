b6fdb022ba2cbcb8f457e9fc09daf3a46f253b15
Serhii911 <serj.roshen@gmail.com>
2022-11-28 14:50:34 +0200

    uppdate class wordsCounter

--- src/WordsCouunter.java
+++ src/WordsCouunter.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -7,7 +6,8 @@ import java.util.Map;
 
 public class WordsCouunter {
 
-    public static Map<String, Integer> counter() {
+    public static String counter() {
+        StringBuilder find = new StringBuilder();
         Map<String, Integer> result = new HashMap<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("words.txt"))) {
             while (bufferedReader.ready()) {
@@ -20,11 +20,20 @@ public class WordsCouunter {
                     }
                 }
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            for(Map.Entry<String, Integer> entry: result.entrySet()) {
+
+                String key = entry.getKey();
+
+                Integer value = entry.getValue();
+                find.append(key).append(" ").append(value).append("\n");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return result;
+        return find.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(counter());
     }
 }
